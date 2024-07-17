c6757c263acbb264178240e3aac0e06d9033eb7d
Robert Buj <robert.buj@gmail.com>
2024-07-17 12:23:06 +0200

    missing import

--- boilerplate-ts/eslint.config.mjs
+++ boilerplate-ts/eslint.config.mjs
@@ -1,6 +1,7 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from 'eslint-config-prettier';
 import html from "eslint-plugin-html";
 import markdown from "eslint-plugin-markdown";
 
